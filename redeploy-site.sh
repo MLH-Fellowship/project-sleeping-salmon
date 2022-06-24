677bad25e2c0767c50733165bcc19b098188d57d
Hailey Moon <82325205+Hailey-moon@users.noreply.github.com>
2022-06-24 12:14:40 -0400

    changed redeploy script to use service instead of tmux

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Kill currently running flask portfolio server
-tmux kill-session -t portfolio
-
 # Makes sure git repo inside the VPS has the latest changes from the main branch on github
 cd Flask-Portfolio
 git fetch && git reset origin/main --hard
@@ -14,9 +11,11 @@ source python3-virtualenv/bin/activate
 # Install python dependenvies
 pip install -r requirements.txt
 
-# start a new detached tmux server and runs the site there
-tmux new -ds portfolio 'python -m venv python3-virtualenv;
-source python3-virtualenv/bin/activate;
-pip install -r requirements.txt;
-flask run --host=0.0.0.0;
-exec $SHELL'
+# Reload changes and restart the service
+systemctl daemon-reload
+systemctl restart myportfolio
+
+# Uncomment to display status of the service
+# systemctl status myportfolio
+# Uncomment to see the full journal
+# journalctl -u myportfolio
